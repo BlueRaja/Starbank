d699b30907ed426798ee4fea8790a7b153f5bbfb
BlueRaja <BlueRaja.admin@gmail.com>
2012-04-08 22:39:04 -0500

    Update installer/version info

--- StarBank/AboutBox.Designer.cs
+++ StarBank/AboutBox.Designer.cs
@@ -55,9 +55,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(19, 43);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.Size = new System.Drawing.Size(155, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Written by BlueRaja 2011";
+            this.label2.Text = "Written by BlueRaja 2011-2012";
             // 
             // label3
             // 
