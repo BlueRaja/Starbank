36249e10f44d97326c4f801badd6f28a0e988b5d
BlueRaja <BlueRaja.admin@gmail.com>
2012-04-11 18:42:33 -0500

    Made map list resizable

--- StarBank/MainForm.Designer.cs
+++ StarBank/MainForm.Designer.cs
@@ -63,22 +63,29 @@
             this.cmbBankFile = new System.Windows.Forms.ComboBox();
             this.panelMultipleBankFiles = new System.Windows.Forms.Panel();
             this.bankEditor1 = new StarBank.BankEditor();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
             this.contextMenuStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.panelMultipleBankFiles.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 28);
+            this.listBox1.Location = new System.Drawing.Point(12, 29);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(214, 372);
+            this.listBox1.Size = new System.Drawing.Size(214, 356);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             this.listBox1.Format += new System.Windows.Forms.ListControlConvertEventHandler(this.listBox1_Format);
@@ -190,9 +197,9 @@
             this.lblMapName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.lblMapName.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMapName.Location = new System.Drawing.Point(269, 28);
+            this.lblMapName.Location = new System.Drawing.Point(14, 13);
             this.lblMapName.Name = "lblMapName";
-            this.lblMapName.Size = new System.Drawing.Size(528, 45);
+            this.lblMapName.Size = new System.Drawing.Size(545, 45);
             this.lblMapName.TabIndex = 1;
             this.lblMapName.Text = "Map name";
             this.lblMapName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -200,7 +207,7 @@
             // lblAuthor
             // 
             this.lblAuthor.AutoSize = true;
-            this.lblAuthor.Location = new System.Drawing.Point(269, 77);
+            this.lblAuthor.Location = new System.Drawing.Point(14, 62);
             this.lblAuthor.Name = "lblAuthor";
             this.lblAuthor.Size = new System.Drawing.Size(64, 13);
             this.lblAuthor.TabIndex = 2;
@@ -209,7 +216,7 @@
             // lblLastUpdate
             // 
             this.lblLastUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblLastUpdate.Location = new System.Drawing.Point(484, 77);
+            this.lblLastUpdate.Location = new System.Drawing.Point(246, 62);
             this.lblLastUpdate.Name = "lblLastUpdate";
             this.lblLastUpdate.Size = new System.Drawing.Size(313, 23);
             this.lblLastUpdate.TabIndex = 3;
@@ -222,7 +229,7 @@
             this.cbxHideBlizzard.AutoSize = true;
             this.cbxHideBlizzard.Checked = true;
             this.cbxHideBlizzard.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbxHideBlizzard.Location = new System.Drawing.Point(12, 410);
+            this.cbxHideBlizzard.Location = new System.Drawing.Point(12, 395);
             this.cbxHideBlizzard.Name = "cbxHideBlizzard";
             this.cbxHideBlizzard.Size = new System.Drawing.Size(116, 17);
             this.cbxHideBlizzard.TabIndex = 5;
@@ -236,7 +243,7 @@
             this.cbxHideMapsWithoutBank.AutoSize = true;
             this.cbxHideMapsWithoutBank.Checked = true;
             this.cbxHideMapsWithoutBank.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbxHideMapsWithoutBank.Location = new System.Drawing.Point(12, 433);
+            this.cbxHideMapsWithoutBank.Location = new System.Drawing.Point(12, 418);
             this.cbxHideMapsWithoutBank.Name = "cbxHideMapsWithoutBank";
             this.cbxHideMapsWithoutBank.Size = new System.Drawing.Size(161, 17);
             this.cbxHideMapsWithoutBank.TabIndex = 6;
@@ -345,9 +352,9 @@
             this.panelMultipleBankFiles.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelMultipleBankFiles.Controls.Add(this.lblMultipleBankFiles);
             this.panelMultipleBankFiles.Controls.Add(this.cmbBankFile);
-            this.panelMultipleBankFiles.Location = new System.Drawing.Point(272, 93);
+            this.panelMultipleBankFiles.Location = new System.Drawing.Point(17, 78);
             this.panelMultipleBankFiles.Name = "panelMultipleBankFiles";
-            this.panelMultipleBankFiles.Size = new System.Drawing.Size(536, 30);
+            this.panelMultipleBankFiles.Size = new System.Drawing.Size(553, 30);
             this.panelMultipleBankFiles.TabIndex = 11;
             this.panelMultipleBankFiles.Visible = false;
             // 
@@ -356,25 +363,56 @@
             this.bankEditor1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.bankEditor1.Location = new System.Drawing.Point(272, 127);
+            this.bankEditor1.Location = new System.Drawing.Point(17, 112);
             this.bankEditor1.Name = "bankEditor1";
-            this.bankEditor1.Size = new System.Drawing.Size(536, 323);
+            this.bankEditor1.Size = new System.Drawing.Size(553, 323);
             this.bankEditor1.TabIndex = 8;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.cbxHideMapsWithoutBank);
+            this.splitContainer1.Panel1.Controls.Add(this.cbxHideBlizzard);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lblMapName);
+            this.splitContainer1.Panel2.Controls.Add(this.panelMultipleBankFiles);
+            this.splitContainer1.Panel2.Controls.Add(this.lblLastUpdate);
+            this.splitContainer1.Panel2.Controls.Add(this.bankEditor1);
+            this.splitContainer1.Panel2.Controls.Add(this.lblAuthor);
+            this.splitContainer1.Size = new System.Drawing.Size(828, 446);
+            this.splitContainer1.SplitterDistance = 231;
+            this.splitContainer1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Map List";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(828, 473);
-            this.Controls.Add(this.panelMultipleBankFiles);
-            this.Controls.Add(this.bankEditor1);
             this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.lblAuthor);
-            this.Controls.Add(this.cbxHideBlizzard);
-            this.Controls.Add(this.lblMapName);
-            this.Controls.Add(this.cbxHideMapsWithoutBank);
-            this.Controls.Add(this.lblLastUpdate);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.splitContainer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MinimumSize = new System.Drawing.Size(836, 500);
@@ -385,6 +423,12 @@
             this.menuStrip1.PerformLayout();
             this.panelMultipleBankFiles.ResumeLayout(false);
             this.panelMultipleBankFiles.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -425,6 +469,8 @@
         private System.Windows.Forms.Label lblMultipleBankFiles;
         private System.Windows.Forms.ComboBox cmbBankFile;
         private System.Windows.Forms.Panel panelMultipleBankFiles;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
 
     }
 }
